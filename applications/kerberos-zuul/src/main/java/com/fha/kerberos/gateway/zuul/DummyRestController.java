c29b6402eddb50dfe067bdd2aa17f575c61c25f3
Oded Shopen <odedia@gmail.com>
2020-01-18 16:21:59 +0200

    Dockerfile

--- applications/kerberos-zuul/src/main/java/com/fha/kerberos/gateway/zuul/DummyRestController.java
+++ applications/kerberos-zuul/src/main/java/com/fha/kerberos/gateway/zuul/DummyRestController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class DemoRestController {
+public class DummyRestController {
 	@GetMapping("/hello")
 	public String hi() {
 		return "hi.";
